0881b09bcf1c44e4a0b4ca535f1eac70b882a512
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2021-08-19 15:37:57 +0300

    Add copying to clipboard (share)

--- game-service-ui/src/app/features/games/components/game-card/game-card.component.ts
+++ game-service-ui/src/app/features/games/components/game-card/game-card.component.ts
@@ -1,24 +1,35 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Game} from "../../../../core/models/game.model";
 import {GamesService} from "../../../../core/services/games-service/games.service";
+import {environment} from "../../../../../environments/environment";
 
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.scss']
 })
-export class GameCardComponent implements OnInit {
+export class GameCardComponent {
   @Input() game!: Game;
   picturePlaceholder = '/assets/image-placeholder-icon.png';
   addedToLibrary = this.game?.addedToCurrentUser || false;
 
   constructor(private gamesService: GamesService) { }
 
-  ngOnInit(): void {
+  private getShortDescription() {
+    return this.game.description.split('. ')[0];
+  }
+
+  private formShareText(): string {
+    return `${this.game.name}: ${this.getShortDescription()}.. Discover more exciting games at ${environment.apiUrl}`;
   }
 
   addToLibrary() {
     this.gamesService.addGameToLibrary$(this.game._id).subscribe();
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.formShareText())
+      .then(() => alert('Game info was copied! Now you can share it in social media'))
   }
 
 }
