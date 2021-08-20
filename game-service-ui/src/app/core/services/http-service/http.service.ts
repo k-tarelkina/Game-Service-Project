07d10885bcbcf853e8b29042e7a69fab5825bb50
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2021-08-20 12:27:28 +0300

    Make some changes to http service

--- game-service-ui/src/app/core/services/http-service/http.service.ts
+++ game-service-ui/src/app/core/services/http-service/http.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observablekt.tarelkina@gmail.com
-} from "rxjs";
+import {Observable} from "rxjs";
 import {environment} from "../../../../environments/environment";
 import {tap} from "rxjs/operators";
 
