876dd09c0613d3c9c723fbf7c6396c64d12ae619
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2021-08-27 12:23:17 +0300

    Fix tests for profile module

--- game-service-ui/src/app/core/services/users-service/users.service.ts
+++ game-service-ui/src/app/core/services/users-service/users.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
-import {FriendModel} from "../../models/friend.model";
-import {map} from "rxjs/operators";
 import {HttpService} from "../http-service/http.service";
 import {UserModel} from "../../models/user.model";
-import {ResponseBody} from "../../models/response.body";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   _USERS_URL = '/api/users';
-  _CURRENT_USER_URL = '/api/users/me';
 
   constructor(private httpService: HttpService<UserModel>) { }
 
   getUsersByUsername(username: string): Observable<UserModel[]> {
     return this.httpService
       .get(this._USERS_URL, {params: {username}}) as Observable<UserModel[]>;
-  }
-
-  updateCurrentUser(data: Partial<UserModel>): Observable<ResponseBody> {
-    return this.httpService.patch(this._CURRENT_USER_URL, data);
   }
 }
--- game-service-ui/src/app/features/profile/components/profile-settings-form/profile-settings-form.component.spec.ts
+++ game-service-ui/src/app/features/profile/components/profile-settings-form/profile-settings-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileSettingsFormComponent } from './profile-settings-form.component';
+import {FormBuilder} from "@angular/forms";
 
 describe('ProfileSettingsFormComponent', () => {
   let component: ProfileSettingsFormComponent;
@@ -8,7 +9,10 @@ describe('ProfileSettingsFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileSettingsFormComponent ]
+      declarations: [ ProfileSettingsFormComponent ],
+      providers: [
+        FormBuilder
+      ]
     })
     .compileComponents();
   });
