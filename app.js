5f59f205d59ba6715fdfef6694e182cb736d5b32
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2021-08-27 18:24:32 +0300

    Fix routing in app (node server)

--- app.js
+++ app.js
@@ -22,25 +22,27 @@ app.use(express
     .static(path
         .join(__dirname, 'game-service-ui/dist/game-service-ui')));
 
-app.get('/', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, 'game-service-ui/dist/game-service-ui/index.html'),
-    );
-});
 app.use('/api/auth', authRouter);
 // for the next routers, auth is required:
 app.use('/api/users', usersRouter);
 app.use('/api/games', gamesRouter);
+//
+
+app.get('/*', (req, res) => {
+  res.sendFile(
+      path.join(__dirname, 'game-service-ui/dist/game-service-ui/index.html'),
+  );
+});
 
 app.use((req, res, next) => {
-    res.status(404).json({message: 'Not found'});
+  res.status(404).json({message: 'Not found'});
 });
 
 app.use((err, req, res, next) => {
-    if (err instanceof GamesServiceError) {
-        return res.status(err.status).json({message: err.message});
-    }
-    res.status(500).json({message: err.message});
+  if (err instanceof GamesServiceError) {
+    return res.status(err.status).json({message: err.message});
+  }
+  res.status(500).json({message: err.message});
 });
 
 module.exports = app;
