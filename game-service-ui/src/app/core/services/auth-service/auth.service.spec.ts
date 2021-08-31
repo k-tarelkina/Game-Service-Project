86feff4889beb218b0101511391924bbb4eec099
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2021-08-31 13:40:43 +0300

    Add tests for AuthService

--- game-service-ui/src/app/core/services/auth-service/auth.service.spec.ts
+++ game-service-ui/src/app/core/services/auth-service/auth.service.spec.ts
@@ -2,17 +2,34 @@ import {TestBed} from '@angular/core/testing';
 import {AuthService} from './auth.service';
 import {HttpService} from '../http-service/http.service';
 import {Router} from '@angular/router';
+import {UserModel} from '../../models/user.model';
+import {of} from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
-  let httpServiceSpy: jasmine.SpyObj<HttpService<any>>;
-  const httpSpy = {};
+
+  let httpServiceSpy: jasmine.SpyObj<HttpService<UserModel>>;
+  const httpSpy= jasmine.createSpyObj('HttpService', ['post', 'patch']);
+
   let routerServiceSpy: jasmine.SpyObj<Router>;
   const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+  const credentials = {
+    email: 'email@email.com',
+    password: 'password',
+  };
+  const user = {
+    ...credentials,
+    _id: '_id',
+    username: 'username',
+    token: 'token',
+  };
+  const CURRENT_USER_URL = '/api/users/me';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        AuthService,
         {
           provide: HttpService,
           useValue: httpSpy,
@@ -24,7 +41,10 @@ describe('AuthService', () => {
       ],
     });
     service = TestBed.inject(AuthService);
-    httpServiceSpy = TestBed.inject(HttpService) as jasmine.SpyObj<HttpService<any>>;
+    httpServiceSpy = TestBed.inject(HttpService) as
+      jasmine.SpyObj<HttpService<any>>;
+    httpServiceSpy.post.and.returnValue(of(user));
+    httpServiceSpy.patch.and.returnValue(of());
     routerServiceSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
@@ -32,10 +52,58 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#logout should log out user correctly', () => {
-    service.logout();
-    expect(service.userValue).toBeNull();
-    const localStorageItem = localStorage.getItem('user');
-    expect(localStorageItem).toBeNull();
+  describe('login', () => {
+    it('#login should log in user correctly', (done) => {
+      service.login(credentials.email, credentials.password)
+          .subscribe((userReturned) => {
+            expect(userReturned).toEqual(user);
+            expect(service.userValue).toEqual(user);
+            done();
+          });
+    });
+
+    it('#login should place user to local storage', (done) => {
+      service.login(credentials.email, credentials.password)
+          .subscribe((user) => {
+            const localStorageItem = localStorage.getItem('user');
+            expect(localStorageItem).not.toBeNull();
+            if (localStorageItem) {
+              const userFromLocalStorage = JSON.parse(localStorageItem);
+              expect(userFromLocalStorage).toEqual(user);
+            }
+            done();
+          });
+    });
+
+    it('#login should navigate to the home page', (done) => {
+      service.login(credentials.email, credentials.password)
+          .subscribe((user) => {
+            expect(routerServiceSpy.navigate).toHaveBeenCalledWith(['']);
+            done();
+          });
+    });
+  });
+
+  describe('logout', () => {
+    it('#logout should log out user correctly', (done) => {
+      service.login(credentials.email, credentials.password)
+          .subscribe(() => done());
+      service.logout();
+      expect(service.userValue).toBeNull();
+      const localStorageItem = localStorage.getItem('user');
+      expect(localStorageItem).toBeNull();
+    });
+
+    it('#logout should navigate to /auth/login', (done) => {
+      service.login(credentials.email, credentials.password)
+          .subscribe(() => done());
+      service.logout();
+      expect(routerServiceSpy.navigate).toHaveBeenCalledWith(['/auth/login']);
+    });
+  });
+
+  it('#updateUser should call PATCH with user datas', () => {
+    service.updateUser(user);
+    expect(httpServiceSpy.patch).toHaveBeenCalledWith(CURRENT_USER_URL, user);
   });
 });
